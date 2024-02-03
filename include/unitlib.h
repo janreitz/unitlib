02b7c65a701807cf60728276654e58d26386ef73
Jan Lucas Reitz <reitz@mmi.rwth-aachen.de>
2024-02-03 19:30:17 +0100

    Add SI units

--- include/unitlib.h
+++ include/unitlib.h
@@ -39,28 +39,6 @@ using DivideDimensions = Dimension<std::ratio_subtract<typename D1::L, typename 
   std::ratio_subtract<typename D1::N, typename D2::N>,
   std::ratio_subtract<typename D1::J, typename D2::J>>;
 
-using Dimensionless = Dimension<>;
-using Length = Dimension<std::ratio<1>>;
-using Mass = Dimension<std::ratio<0>, std::ratio<1>>;
-using Time = Dimension<std::ratio<0>, std::ratio<0>, std::ratio<1>>;
-using ElectricCurrent = Dimension<std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<1>>;
-using Temperature = Dimension<std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<1>>;
-using AmountOfSubstance =
-  Dimension<std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<1>>;
-using LuminousIntensity =
-  Dimension<std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<1>>;
-
-using Area = MultiplyDimensions<Length, Length>;
-using Volume = MultiplyDimensions<Length, Area>;
-using Frequency = DivideDimensions<Dimensionless, Time>;
-using Velocity = DivideDimensions<Length, Time>;
-using Acceleration = DivideDimensions<Velocity, Time>;
-using Force = MultiplyDimensions<Mass, Acceleration>;
-using Pressure = DivideDimensions<Force, Area>;
-using Energy = MultiplyDimensions<Force, Length>;
-using Power = DivideDimensions<Energy, Time>;
-
-
 template<typename Dimension, typename ValueType, typename ScalingFactor = std::ratio<1>>
   requires std::integral<ValueType> || std::floating_point<ValueType>
 class Unit
@@ -132,6 +110,41 @@ public:
   }
 };
 
+// Dimensions of SI base units
+using Dimensionless = Dimension<>;
+using Length = Dimension<std::ratio<1>>;
+using Mass = Dimension<std::ratio<0>, std::ratio<1>>;
+using Time = Dimension<std::ratio<0>, std::ratio<0>, std::ratio<1>>;
+using ElectricCurrent = Dimension<std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<1>>;
+using Temperature = Dimension<std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<1>>;
+using AmountOfSubstance =
+  Dimension<std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<1>>;
+using LuminousIntensity =
+  Dimension<std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<0>, std::ratio<1>>;
+
+// Derived Dimensions
+using Area = MultiplyDimensions<Length, Length>;
+using Volume = MultiplyDimensions<Length, Area>;
+using Frequency = DivideDimensions<Dimensionless, Time>;
+using Velocity = DivideDimensions<Length, Time>;
+using Acceleration = DivideDimensions<Velocity, Time>;
+using Force = MultiplyDimensions<Mass, Acceleration>;
+using Pressure = DivideDimensions<Force, Area>;
+using Energy = MultiplyDimensions<Force, Length>;
+using Power = DivideDimensions<Energy, Time>;
+using ElectricCharge = MultiplyDimensions<Time, ElectricCurrent>;
+using ElectricPotential = DivideDimensions<Power, ElectricCurrent>;
+using ElectricalResistance = DivideDimensions<ElectricPotential, ElectricCurrent>;
+using ElectricalConductance = DivideDimensions<Dimensionless, ElectricalResistance>;
+using Capacitance = DivideDimensions<ElectricCharge, ElectricPotential>;
+using MagneticFlux = MultiplyDimensions<ElectricPotential, Time>;
+using MagneticFluxDensity = DivideDimensions<MagneticFlux, Area>;
+using Inductance = DivideDimensions<MagneticFlux, ElectricCurrent>;
+using LuminousFlux = LuminousIntensity;
+using Illuminance = DivideDimensions<LuminousIntensity, Area>;
+using RadioactiveDose = DivideDimensions<Energy, Mass>;
+using KatalyticActivity = DivideDimensions<AmountOfSubstance, Time>;
+
 // SI base units
 template<typename ValueType> using Meter = Unit<Length, ValueType>;
 template<typename ValueType> using Kilogram = Unit<Mass, ValueType>;
@@ -141,8 +154,32 @@ template<typename ValueType> using Kelvin = Unit<Temperature, ValueType>;
 template<typename ValueType> using Mole = Unit<AmountOfSubstance, ValueType>;
 template<typename ValueType> using Candela = Unit<LuminousIntensity, ValueType>;
 
+// SI derived units
+template<typename ValueType> using Radian = Unit<Dimensionless, ValueType>;
+template<typename ValueType> using Steradian = Unit<Dimensionless, ValueType>;
+template<typename ValueType> using Hertz = Unit<Frequency, ValueType>;
+template<typename ValueType> using Newton = Unit<Force, ValueType>;
+template<typename ValueType> using Pascal = Unit<Pressure, ValueType>;
 template<typename ValueType> using Joule = Unit<Energy, ValueType>;
+template<typename ValueType> using Watt = Unit<Power, ValueType>;
+template<typename ValueType> using Coulomb = Unit<ElectricCharge, ValueType>;
+template<typename ValueType> using Volt = Unit<ElectricPotential , ValueType>;
+template<typename ValueType> using Farad = Unit<Capacitance, ValueType>;
+template<typename ValueType> using Ohm = Unit<ElectricalResistance, ValueType>;
+template<typename ValueType> using Siemens = Unit<ElectricalConductance , ValueType>;
+template<typename ValueType> using Weber = Unit<MagneticFlux , ValueType>;
+template<typename ValueType> using Tesla = Unit<MagneticFluxDensity , ValueType>;
+template<typename ValueType> using Henry = Unit<Inductance, ValueType>;
+// TODO: How to handle units with offsets?
+// template<typename ValueType> using DegreeCelsius = Unit<Temperature , ValueType>; // Celsius	°C	temperature relative to 273.15 K	K	
+template<typename ValueType> using Lumen = Unit<LuminousFlux, ValueType>;
+template<typename ValueType> using Lux = Unit<Illuminance, ValueType>;
+template<typename ValueType> using Becquerel = Unit<RadioactiveDose, ValueType>;
+template<typename ValueType> using Gray = Unit<RadioactiveDose, ValueType>;
+template<typename ValueType> using Sievert = Unit<RadioactiveDose, ValueType>;
+template<typename ValueType> using Katal = Unit<KatalyticActivity, ValueType>;
 
+// Scaled units
 template<typename ValueType> using Centimeter = Unit<Length, ValueType, std::ratio<1, 100>>;
 template<typename ValueType> using Kilometer = Unit<Length, ValueType, std::ratio<1000>>;
 template<typename ValueType> using Millisecond = Unit<Time, ValueType, std::ratio<1, 1000>>;
