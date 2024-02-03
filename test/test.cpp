0fc357e6a7c55c95ab95180a0875aa4ac28da03f
Jan Lucas Reitz <reitz@mmi.rwth-aachen.de>
2024-02-03 20:05:30 +0100

    Use compiletime checks for testing instead of iostream

--- test/test.cpp
+++ test/test.cpp
@@ -1,39 +1,32 @@
 #include "../include/unitlib.h"
 
-#include <iostream>
-
 using namespace Unitlib;
 
 int main(int, char **)
 {
-  constexpr Meter<double> distance(100);
-  constexpr Kilometer<double> distance_km = distance;
-  constexpr Centimeter<double> addition_result = distance + distance_km;
-  constexpr Meter<double> subtraction_result = distance - distance_km;
 
-  std::cout << "Centimeter: " << double(Centimeter<double>(distance) * 5) << '\n';
-  std::cout << "Kilometer: " << double(Kilometer<double>(distance)) << '\n';
-  std::cout << "Addition result: " << double(addition_result) << '\n';
-  std::cout << "Subtraction result: " << double(subtraction_result) << '\n';
+  // Test for basic instantiation and value retrieval
+  constexpr Meter<double> oneMeter(1.0);
+  constexpr Kilogram<double> oneKilogram(1.0);
+  constexpr Second<double> oneSecond(1.0);
+  constexpr Ampere<double> oneAmpere(1.0);
+  constexpr Kelvin<double> oneKelvin(1.0);
+  constexpr Mole<double> oneMole(1.0);
+  constexpr Candela<double> oneCandela(1.0);
+  static_assert(oneMeter.getValue() == 1.0, "One meter should have a value of 1.0");
 
-  constexpr Second<double> time(30);
-  constexpr Millisecond<double> time_ms = time;
-  constexpr Hour<double> time_h = time;
-  std::cout << double(time_ms) << '\n';
+  // Test for dimensional analysis - multiplication
+  constexpr Meter<double> length(2.0);
+  constexpr Unit<Area, double> squareMeter = length * length;
+  static_assert(squareMeter.getValue() == 4.0, "Area calculation failed");
 
-  auto area = distance * distance;
-  std::cout << "Square meters" << double(area) << '\n';
+  // Test for unit conversion
+  constexpr Kilometer<double> oneMeterInKilometers = oneMeter;
+  static_assert(oneMeterInKilometers.getValue() == 0.001, "Meter to Kilometer conversion failed");
 
-  constexpr Unit<Velocity, double> meters_per_second = distance_km / time_h;
-
-  auto kilometers_per_hour = distance_km / time_h;
-
-  std::cout << double(distance_km) << " [km] / " << double(time_h) << " [h] = " << double(meters_per_second) << "[m/s]"
-            << '\n';
-
-  std::cout << double(distance_km) << " [km] / " << double(time_h) << " [h] = " << double(kilometers_per_hour)
-            << "[km/h]" << '\n';
-
+  // Test for arithmetic operations
+  constexpr Meter<double> twoMeters = oneMeter + oneMeter;
+  static_assert(twoMeters.getValue() == 2.0, "Addition failed");
 
   return 0;
 }
