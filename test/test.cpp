d6bf954a835cdd7122abc174324256e230295e46
Jan Lucas Reitz <reitz@mmi.rwth-aachen.de>
2024-02-03 19:58:36 +0100

    use using namespace Unitlib in test code

--- test/test.cpp
+++ test/test.cpp
@@ -2,27 +2,29 @@
 
 #include <iostream>
 
+using namespace Unitlib;
+
 int main(int, char **)
 {
-  constexpr Unitlib::Meter<double> distance(100);
-  constexpr Unitlib::Kilometer<double> distance_km = distance;
-  constexpr Unitlib::Centimeter<double> addition_result = distance + distance_km;
-  constexpr Unitlib::Meter<double> subtraction_result = distance - distance_km;
+  constexpr Meter<double> distance(100);
+  constexpr Kilometer<double> distance_km = distance;
+  constexpr Centimeter<double> addition_result = distance + distance_km;
+  constexpr Meter<double> subtraction_result = distance - distance_km;
 
-  std::cout << "Centimeter: " << double(Unitlib::Centimeter<double>(distance) * 5) << '\n';
-  std::cout << "Kilometer: " << double(Unitlib::Kilometer<double>(distance)) << '\n';
+  std::cout << "Centimeter: " << double(Centimeter<double>(distance) * 5) << '\n';
+  std::cout << "Kilometer: " << double(Kilometer<double>(distance)) << '\n';
   std::cout << "Addition result: " << double(addition_result) << '\n';
   std::cout << "Subtraction result: " << double(subtraction_result) << '\n';
 
-  constexpr Unitlib::Second<double> time(30);
-  constexpr Unitlib::Millisecond<double> time_ms = time;
-  constexpr Unitlib::Hour<double> time_h = time;
+  constexpr Second<double> time(30);
+  constexpr Millisecond<double> time_ms = time;
+  constexpr Hour<double> time_h = time;
   std::cout << double(time_ms) << '\n';
 
   auto area = distance * distance;
   std::cout << "Square meters" << double(area) << '\n';
 
-  constexpr Unitlib::Unit<Unitlib::Velocity, double> meters_per_second = distance_km / time_h;
+  constexpr Unit<Velocity, double> meters_per_second = distance_km / time_h;
 
   auto kilometers_per_hour = distance_km / time_h;
 
